7451c09b53a915f35d417f5a2be4b7f6983ff53c
Justin Mahar <contact@justinmahar.com>
2024-02-08 16:19:47 -0500

    Run updater

--- dist/updateObject.d.ts
+++
@@ -1,2 +0,0 @@
-import { FieldUpdate, FieldUpdateOptions, Reassignment } from './LockblocksSettings';
-export declare const updateObject: (originObj: Record<string, any>, targetObj: Record<string, any>, rootUpdateOptions: FieldUpdateOptions, renameFields: Reassignment[], updateFields: FieldUpdate[], deleteFields: string[]) => Record<string, any>;
